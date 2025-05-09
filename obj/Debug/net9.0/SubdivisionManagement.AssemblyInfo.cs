dd1320a3baece9510c7ed40c530be37ed269b704
deLuna18 <alexahontiveros494@gmail.com>
2025-05-09 23:48:59 +0800

    update

--- obj/Debug/net9.0/SubdivisionManagement.AssemblyInfo.cs
+++ obj/Debug/net9.0/SubdivisionManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SubdivisionManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b1a2fedd9faa666731db8c6ef0caf24698ebb2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd1e36a63402a4d85a3722e1f0ec2c64b15a1ba5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SubdivisionManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SubdivisionManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
